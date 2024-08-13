a08da20b09dc6cd5900b8214cdde64c0df4e5d56
Ana Ramirez <anita-0621@hotmail.com>
2024-08-13 10:02:17 -0500

    WIP

---
+++ tailwind-implementation/src/App.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-slate-400">Hola es el titulo</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind-implementation/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0B0D17",
+        secondary: "#D0D6F9",
+        tertiary: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
